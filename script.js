8d660b5051afa57efced27a41a30125a302fff64
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-03 16:36:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,17 +5,8 @@ function firstWord(s) {
     return s.substring(0, s.indexOf(' '));
   }
 }
-	
-    /*for (let i = 0; i < s.length; i++) {
-        if (s[i] === ' ') {
-            return s.substring(0, i);
-        }
-    }
-    return s;  // Return the entire string if no space is found*/
-
-
-
-// Do not change the code below
-
- const s = prompt("Enter String:");
-alert(firstWord(s));
+// Test cases
+console.log(firstWord('see and stop'));   // Output: 'see'
+console.log(firstWord(' Hello World!'));  // Output: 'Hello'
+console.log(firstWord('12345'));          // Output: '12345'
+console.log(firstWord(''));               // Output: ''
